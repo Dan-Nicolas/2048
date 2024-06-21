f5af2fc6038d588243d1bf842b8ddae3562b99cb
Dan-Nicolas <70666118+Dan-Nicolas@users.noreply.github.com>
2024-06-20 23:31:29 -0400

    added comments

--- main.py
+++ main.py
@@ -89,6 +89,9 @@ def draw(window, tiles):
     pg.display.update()
 
 def get_random_pos(tiles):
+    """
+    generates a random (row,col) that isn't already occupied for a tile
+    """
     row, col = None, None
     while True:
         row = random.randrange(0, ROWS)
@@ -99,6 +102,11 @@ def get_random_pos(tiles):
     return row,col
 
 def update_tiles(window, tiles, sorted_tiles):
+    """
+    'sorted_tiles' used in 'move_tiles' is a sorted version of 'tiles' which is only temporarily used
+
+    here 'tiles' is wiped clean and the contents of 'sorted_tiles' is assigned to 'tiles'
+    """
     tiles.clear()
     for tile in sorted_tiles:
         tiles[f"{tile.row}{tile.col}"] = tile
@@ -106,28 +114,57 @@ def update_tiles(window, tiles, sorted_tiles):
     draw(window,tiles)
 
 def end_move(tiles):
+    """
+    when a move ends do the following:
+        check if the board is full; game over if it is
+        generate a new tile in random unoccupied space
+    """
     if len(tiles) == 16:
         return "lost"
     
     row, col = get_random_pos(tiles)
-    tiles[f"{row}{col}"] = Tile(random.choice([2,4]), row, col)
+    val = random.randrange(1,10)
+    if val <= 8:
+        val = 2
+    else:
+        val = 4
+    tiles[f"{row}{col}"] = Tile(val, row, col)
     return "continue"
 
 def move_tiles(window, tiles, clock, direction):
+    """
+    Moves tiles in the direction inputs pressed and merges tiles when necessary
+    """
     updated = True
+    # tells us which blocks have already merged in a movement bc we dont want to merge multiple sets of tiles
+    # i.e. we dont allow merged blocks to continuously merge in one movement: 2:2 -> 4:4 -> 8
     blocks = set()
 
     if direction == "left":
+        # sort the tiles from left to right so the leftest tile moves left first 
+        # followed by the second most leftest and so on
         sort_func = lambda x: x.col
+
         reverse = False
+
+        # specifies how much we want to move each tile by each frame
         delta = (-MOVE_SPEED, 0)
+
+        # if the column is at 0 then the tile is as far left as it can go
         boundary_check = lambda tile: tile.col == 0
+
+        # returns the tile to the left if there is one 
+        # to check if the current tile will be blocked or merge
         get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col - 1}")
+
+        # checks if a merge should occur based on the current movement
         merge_check = lambda tile, next_tile: tile.x > next_tile.x + MOVE_SPEED
-        move_check = (
-            lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_SPEED
-        )
+
+        # if the tile to the left isnt the same value stop moving to the right of it (RECT_WIDTH)
+        move_check = lambda tile, next_tile: tile.x > next_tile.x + RECT_WIDTH + MOVE_SPEED
+        
         ceil = True
+
     elif direction == "right":
         sort_func = lambda x: x.col
         reverse = True
@@ -135,10 +172,9 @@ def move_tiles(window, tiles, clock, direction):
         boundary_check = lambda tile: tile.col == COLS - 1
         get_next_tile = lambda tile: tiles.get(f"{tile.row}{tile.col + 1}")
         merge_check = lambda tile, next_tile: tile.x < next_tile.x - MOVE_SPEED
-        move_check = (
-            lambda tile, next_tile: tile.x + RECT_WIDTH + MOVE_SPEED < next_tile.x
-        )
+        move_check = lambda tile, next_tile: tile.x + RECT_WIDTH + MOVE_SPEED < next_tile.x
         ceil = False
+
     elif direction == "up":
         sort_func = lambda x: x.row
         reverse = False
@@ -146,10 +182,9 @@ def move_tiles(window, tiles, clock, direction):
         boundary_check = lambda tile: tile.row == 0
         get_next_tile = lambda tile: tiles.get(f"{tile.row - 1}{tile.col}")
         merge_check = lambda tile, next_tile: tile.y > next_tile.y + MOVE_SPEED
-        move_check = (
-            lambda tile, next_tile: tile.y > next_tile.y + RECT_HEIGHT + MOVE_SPEED
-        )
+        move_check = lambda tile, next_tile: tile.y > next_tile.y + RECT_HEIGHT + MOVE_SPEED
         ceil = True
+
     elif direction == "down":
         sort_func = lambda x: x.row
         reverse = True
@@ -157,9 +192,7 @@ def move_tiles(window, tiles, clock, direction):
         boundary_check = lambda tile: tile.row == ROWS - 1
         get_next_tile = lambda tile: tiles.get(f"{tile.row + 1}{tile.col}")
         merge_check = lambda tile, next_tile: tile.y < next_tile.y - MOVE_SPEED
-        move_check = (
-            lambda tile, next_tile: tile.y + RECT_HEIGHT + MOVE_SPEED < next_tile.y
-        )
+        move_check = lambda tile, next_tile: tile.y + RECT_HEIGHT + MOVE_SPEED < next_tile.y
         ceil = False
 
     while updated:
@@ -224,13 +257,13 @@ def main(window):
                 break
 
             if event.type == pg.KEYDOWN:
-                if event.key == pg.K_LEFT:
+                if event.key == pg.K_LEFT or event.key == pg.K_a:
                     move_tiles(window, tiles, clock, "left")
-                if event.key == pg.K_RIGHT:
+                if event.key == pg.K_RIGHT or event.key == pg.K_d:
                     move_tiles(window, tiles, clock, "right")
-                if event.key == pg.K_UP:
+                if event.key == pg.K_UP or event.key == pg.K_w:
                     move_tiles(window, tiles, clock, "up")
-                if event.key == pg.K_DOWN:
+                if event.key == pg.K_DOWN or event.key == pg.K_s:
                     move_tiles(window, tiles, clock, "down")
         draw(WINDOW, tiles)
 
